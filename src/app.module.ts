a99338aa4902753dfa5cc0ebd9c255d8d01d221b
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-06-24 13:40:09 +0500

    feat(app): register AuthModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule,
-    DatabaseModule,
+    DatabaseModule,AuthModule
   ],
 })
 export class AppModule {}
