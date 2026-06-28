11065d188cc20a568b4d9f0accc5de816853ac5e
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-06-29 00:29:25 +0500

    feat(app): register UsersModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
     ConfigModule,
-    DatabaseModule,AuthModule
+    DatabaseModule,AuthModule,UsersModule,
   ],
 })
 export class AppModule {}
