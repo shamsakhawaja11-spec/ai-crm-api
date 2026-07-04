49131a8fd2eefe62f33b09e7f3f4dff31da1a9d6
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-07-04 21:14:43 +0500

    feat(app): register TeamsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
+import { TeamsModule } from './modules/teams/teams.module';
 
 @Module({
   imports: [
     ConfigModule,
-    DatabaseModule,AuthModule,UsersModule,
+    DatabaseModule,AuthModule,UsersModule,TeamsModule,
   ],
 })
 export class AppModule {}
